87d7af057dfc87e6b1f6418b7222f325e010e979
GiusepeBecchettiScuola <119665915+GiusepeBecchettiScuola@users.noreply.github.com>
2023-12-06 13:29:59 +0100

    upgrade sjn

--- TPSIT/src/sjn/SchedulerSjn.java
+++ TPSIT/src/sjn/SchedulerSjn.java
@@ -15,7 +15,7 @@ public class SchedulerSjn {
 		processo[1]="P1";
 		processo[2]="P2";
 		processo[3]="P3";
-		
+
 		//		tExe[0]=(int) (Math.random()*10)+1;
 		//		tExe[1]=(int) (Math.random()*10)+1;
 		//		tExe[2]=(int) (Math.random()*10)+1;
@@ -24,7 +24,7 @@ public class SchedulerSjn {
 		tExe[1]=3;
 		tExe[2]=8;
 		tExe[3]=6;
-		
+
 		tArrivo[0]=0;
 		tArrivo[1]=1;
 		tArrivo[2]=2;
@@ -34,26 +34,41 @@ public class SchedulerSjn {
 			System.out.println("Processo: "+processo[i]);
 			System.out.println("Tempo di arrivo: "+tArrivo[i]);
 			System.out.println("Tempo di Esecuzione:"+tExe[i]);
+		
+		for(int superficie=0;superficie<=tExe.length-1-1;superficie=superficie+1){
+			for(int posizione=tExe.length-1-1;posizione>=superficie;posizione--){
+				if(tExe[posizione]>tExe[posizione+1]){
 
-			for(int superficie=0;superficie<=tExe.length-1-1;superficie=superficie+1){
-				for(int posizione=tExe.length-1-1;posizione>=superficie;posizione--){
-					if(tExe[posizione]>tExe[posizione+1]){
-						int appoggio=tExe[posizione];
-						tExe[posizione]=tExe[posizione+1];
-						tExe[posizione+1]=appoggio;
-					}
-					if(tArrivo[posizione]>tArrivo[posizione+1]){
-						int appoggio2=tArrivo[posizione];
-						tArrivo[posizione]=tArrivo[posizione+1];
-						tArrivo[posizione+1]=appoggio2;
-					}
+					int appoggioExe=tExe[posizione];
+					tExe[posizione]=tExe[posizione+1];
+					tExe[posizione+1]=appoggioExe;
+
+					String appoggioPro=processo[posizione];
+					processo[posizione]=processo[posizione+1];
+					processo[posizione+1]=appoggioPro;
+
+					int appoggioArrivo=tArrivo[posizione];
+					tArrivo[posizione]=tArrivo[posizione+1];
+					tArrivo[posizione+1]=appoggioArrivo;
+
+					//						int appoggioEsecuzione=esecuzione[posizione];
+					//						String appoggioProcesso = processo[posizione];
+					//						int appoggioArrivo = arrivo[posizione];
+					//						
+					//						esecuzione[posizione]=esecuzione[posizione+1];
+					//						processo[posizione]=processo[posizione+1];
+					//						arrivo[posizione]=arrivo[posizione+1];
+					//						
+					//						esecuzione[posizione+1]=appoggioEsecuzione;
+					//						processo[posizione+1]=appoggioProcesso;
+					//						arrivo[posizione+1]=appoggioArrivo;					
 				}
 			}
+		}
 
 			if(tArrivo[i]==0) {
 				tServizio[i]=0;
 				tAttesa[i]=0;
-
 			}else {
 				tServizio[i]=tServizio[i-1]+tExe[i-1];
 				tAttesa[i]=tServizio[i]-tArrivo[i];
@@ -61,7 +76,7 @@ public class SchedulerSjn {
 			}
 			System.out.println("Tempo di servizio: "+tServizio[i]);
 			System.out.println("Tempo di attesa: "+tAttesa[i]);
-		}
+	}
 		System.out.println("Media tempi di attesa: "+media);
 	}	
 }
